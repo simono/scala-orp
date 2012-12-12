3c38e5453d32f11e940bfae6eee08a74d2c6c683
Simon Olofsson <simon@olofsson.de>
2012-12-12 21:55:31 +0100

    Update to Scala 2.10.0-RC5.

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ import Keys._
  */
 object BuildSettings {
   val buildOrganization = "orp"
-  val buildScalaVersion = "2.10.0-RC3"
+  val buildScalaVersion = "2.10.0-RC5"
   val buildVersion = "1.0"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
